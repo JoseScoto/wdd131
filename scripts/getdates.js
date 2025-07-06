79ebf346948e1496698eb8ffd95267150f69a8e9
JoseScoto <esc16004@byui.edu>
2025-07-05 20:55:36 -0600

    First Week done

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get current year and populate the copyright year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Get last modified date and populate the last modified paragraph
+document.getElementById('lastModified').textContent = 'Last Modification: ' + document.lastModified;
--- test.js
+++
@@ -1,8 +0,0 @@
-{
-    "hello": {
-        "scope": "javascript,html",
-            "prefix": "hello",
-                "body": "$BLOCK_COMMENT_START Hello World $BLOCK_COMMENT_END"
-    }
-}
-  
