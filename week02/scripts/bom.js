0a0760edc10d3159c1ebbb1bfbd6774ea64db43c
JoseScoto <esc16004@byui.edu>
2025-07-17 19:28:38 -0600

    BoM Activity Preparation

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,3 +1,9 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('list');
+
+// Li element to hold the chapter
+const li = document.createElement('li');
+
+// Creating a delete button
+const deleteButton = document.createElement('button');
