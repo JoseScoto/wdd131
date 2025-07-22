ef15f503f06ce3f61c7556ebc8d37e638388e3f4
JoseScoto <esc16004@byui.edu>
2025-07-21 18:18:14 -0600

    BofM Complete

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,21 +1,39 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('list');
+const list = document.querySelector('#list');
 
-// Li element to hold the chapter
-const li = document.createElement('li');
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') 
+        {
+        // Li element to hold the chapter
+        const li = document.createElement('li');
 
-// Creating a delete button
-const deleteButton = document.createElement('button');
+        // Creating a delete button
+        const deleteButton = document.createElement('button');
 
-// Li element variable's textContent or innerHTML
-li.textContent = input.value;
+        // Li element variable's textContent or innerHTML
+        li.textContent = input.value;
 
-// Populate the delete button
-deleteButton.textContent = '❌';
+        // Populate the delete button
+        deleteButton.textContent = '❌';
 
-// Append the delete button to the li element
-li.append(deleteButton);
+        // Add an event listener to the delete button
+        deleteButton.addEventListener('click', function () {
+            // Remove the li element when the delete button is clicked
+            list.removeChild(li);
+            input.focus();
+        }
+        );
 
-// Append the li element to the list
-list.append(li);
+        // Append the delete button to the li element
+        li.append(deleteButton);
+
+        // Append the li element to the list
+        list.append(li);
+
+        input.value = '';
+
+        input.focus();
+        }
+        return;
+});
